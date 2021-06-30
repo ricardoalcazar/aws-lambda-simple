8b1e14eab59722738b851ed0fa977cc01a70d183
Ricardo Alcazar <ricardo.alcazar@myqops.com>
2021-06-30 15:29:30 -0500

    [MOD] Added README and updated Handler.

---
+++ README.md
@@ -0,0 +1,7 @@
+Author: R. Alcazar
+Modified: 6/30/2021
+
+This Maven project establishes a basic framework for Amazon Lambda Request Handler.
+It contains a request handler that uses a logger object to output messages to Amazon CloudWatch associated with Lambda execution.
+
+*GitHub:* https://github.com/ricardoalcazar/aws-lambda-simple
--- src/digital/alcazar/aws/LambdaRequestHandler.java
+++ src/digital/alcazar/aws/LambdaRequestHandler.java
@@ -1,34 +1,34 @@
 package digital.alcazar.aws;
 
-
-
-import org.slf4j.*;
 import java.util.*;
 import com.amazonaws.services.lambda.runtime.*;
 import com.google.gson.*;
 
 
-
-
 public class LambdaRequestHandler implements RequestHandler<Map<String,String>, String>{
 	
-	
-	private static final Logger LOGGER = LoggerFactory.getLogger(Tester.class);
-	
 	public String handleRequest(Map<String,String> event, Context context){
+		
+		
+		// Google JSON parser
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		
+		// Access CloudWatch logger object
 	    LambdaLogger logger = context.getLogger();
 	    String response = new String("200 OK");
 	    
 	    
-	    // log execution details
+	    // Log execution details to CloudWatch logs
 	    logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
 	    logger.log("CONTEXT: " + gson.toJson(context));
-	    // process event
+	    
+	    // Log process event details
 	    logger.log("EVENT: " + gson.toJson(event));
 	    logger.log("EVENT TYPE: " + event.getClass().toString());
 	    return response;
-	  }
+	    
+	    
+	}
 	
 
 }
